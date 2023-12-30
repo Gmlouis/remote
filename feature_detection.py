71ad2cea290a51d27e530d0e21f4bf170302709b
Gmlouis <gmlouis33@gmail.com>
2023-12-30 18:40:57 -0500

    debugging

--- feature_detection.py
+++ feature_detection.py
@@ -1,7 +1,7 @@
-import cv2
 import numpy as np
+import cv2
 
-class SIFTFeatureDetector(feature_detection):
+class SIFTFeatureDetector(FeatureDetector):
     def __init__(self):
         self.sift = cv2.xfeatures2d.SIFT_create()
         self.blur_processor = GaussianBlurProcessor()
@@ -13,11 +13,18 @@ class SIFTFeatureDetector(feature_detection):
         descriptors_list = []
 
         for image in images:
-            gray = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
+            image_copy = image.copy()
+            gray = cv2.cvtColor(np.array(image_copy), cv2.COLOR_RGB2GRAY)
             blurred = self.blur_processor.process(gray)
             enhanced = self.edge_processor.process(blurred)
             stretched = self.contrast_processor.process(enhanced)
             keypoints, descriptors = self.sift.detectAndCompute(stretched, None)
+
+        for keypoint in keypoints:
+            y, x = keypoint.pt
+            y, x = int(round(y)), int(round(x))
+            keypoint.pt = (x, y)
+
             keypoints_list.append(keypoints)
             descriptors_list.append(descriptors)
 
