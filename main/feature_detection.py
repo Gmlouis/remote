5387ba1226e6b4053c243940f40d05ddb33cbcf0
Gmlouis <gmlouis33@gmail.com>
2024-01-02 18:16:13 -0500

    feature detection corrections

--- main/feature_detection.py
+++ main/feature_detection.py
@@ -1,32 +1,22 @@
-import numpy as np
 import cv2
-from feature_enhancement import GaussianBlurProcessor, EdgeEnhancementProcessor, ContrastStretchingProcessor
+import numpy as np
 
-class SIFTFeatureDetector:
-    def __init__(self):
-        self.sift = cv2.xfeatures2d.SIFT_create()
-        self.blur_processor = GaussianBlurProcessor()
-        self.edge_processor = EdgeEnhancementProcessor()
-        self.contrast_processor = ContrastStretchingProcessor()
+def detect_features(self, images):
+    keypoints_list = []
+    descriptors_list = []
 
-    def detect_features(self, images):
-        keypoints_list = []
-        descriptors_list = []
+    for i, image in enumerate(images):
+        image_copy = image.copy()
+        gray = cv2.cvtColor(np.array(image_copy), cv2.COLOR_RGB2GRAY)
+        blurred = self.blur_processor.process(gray)
+        enhanced = self.edge_processor.process(blurred)
+        stretched = self.contrast_processor.process(enhanced)
+        keypoints, descriptors = self.sift.detectAndCompute(stretched, None)
 
-        for image in images:
-            image_copy = image.copy()
-            gray = cv2.cvtColor(np.array(image_copy), cv2.COLOR_RGB2GRAY)
-            blurred = self.blur_processor.process(gray)
-            enhanced = self.edge_processor.process(blurred)
-            stretched = self.contrast_processor.process(enhanced)
-            keypoints, descriptors = self.sift.detectAndCompute(stretched, None)
+        keypoints_list.append(keypoints)
+        descriptors_list.append(descriptors)
 
-        for keypoint in keypoints:
-            y, x = keypoint.pt
-            y, x = int(round(y)), int(round(x))
-            keypoint.pt = (x, y)
+        # Draw keypoints on the original image and replace it in the list
+        images[i] = cv2.drawKeypoints(image, keypoints, outImage=np.array([]), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-            keypoints_list.append(keypoints)
-            descriptors_list.append(descriptors)
-
-        return keypoints_list, descriptors_list
+    return keypoints_list, descriptors_list
